3d10ae09f8e7097e0e71daa18cb07d0ca0cd04c9
toby <toby@swinson-howell.net>
2022-09-17 07:37:39 +0100

    started working

--- make_info.py
+++ make_info.py
@@ -5,10 +5,10 @@ info = json.load(open("info.json"))
 print(info, type(info))
 
 for planet in info: # loop through planets
-    output.write(f"<button onclick=\"switch_to('{planet}')\" id=\"{planet}-button\" class=\"planet-buttons\"><img src=\"images/{planet}.png\"></button>")
+    output.write(f"<button onclick=\"switch_to('{planet}')\" id=\"{planet}-button\" class=\"planet-buttons\"><img src=\"images/{planet}.png\" class=\"planet-button-image\"></button>")
 for planet in info:
     output.write(f"<div id=\"{planet}-info\" class=\"planet-info\">")
     print(info[planet])
     for key in info[planet]: # loop through info
-        output.write(f"{key}: {info[planet][key]} <br>")
-    output.write("</div>")
+        output.write(f"<strong>{key}</strong>: {info[planet][key]} <br>")
+    output.write(f"<img class=\"planet-image\" id={planet}-image src=\"images/{planet}.png\"></div>")
--- simple_info.js
+++ simple_info.js
@@ -2,10 +2,17 @@ function switch_to(thing) {
     // $(document.getElementsByClassName(".highlighted-planet-button")).attr("class", ".planet-button");
     // console.log($(`#${thing}-button`), $(".highlighted-planet-button"), document.getElementsByClassName(".highlighted-planet-button"));
     // $(`#${thing}-button`).addClass(".highlighted-planet-button");
-    $(".planet-buttons").css("border", "none")
+
+
+    // $(".planet-buttons").css("border", "none")
+    // $(`#${thing}-button`).css({
+    //     "border": "2px solid red",
+    //     "border-radius": "5px"
+    // });
+
+    $(".planet-buttons").css("width", "9.5%")
     $(`#${thing}-button`).css({
-        "border": "2px solid red",
-        "border-radius": "5px"
+        "width": "10%"
     });
     $(".planet-info").hide(0);
     $(`#${thing}-info`).show(0);
