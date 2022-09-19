71981d50371b0ffd66e8f08a0d2295ed0899f288
toby <toby@swinson-howell.net>
2022-09-19 07:02:59 +0100

    info + styling

--- make_info.py
+++ make_info.py
@@ -7,8 +7,11 @@ print(info, type(info))
 for planet in info: # loop through planets
     output.write(f"<button onclick=\"switch_to('{planet}')\" id=\"{planet}-button\" class=\"planet-buttons\"><img src=\"images/{planet}.png\" class=\"planet-button-image\"></button>")
 for planet in info:
-    output.write(f"<div id=\"{planet}-info\" class=\"planet-info\">")
+    output.write(f"<div id=\"{planet}-info\" class=\"planet-info\">\n   <span class=\"planet-name\" id=\"{planet}-name\">{planet.title()}</span>\n")
     print(info[planet])
     for key in info[planet]: # loop through info
-        output.write(f"<strong>{key}</strong>: {info[planet][key]} <br>")
-    output.write(f"<img class=\"planet-image\" id={planet}-image src=\"images/{planet}.png\"></div>")
+        if key == "Intro":
+            output.write(f'<span class="planet-intro" id="{planet}-intro">{info[planet]["Intro"]}</span>')
+        else:
+            output.write(f"    <strong>{key}</strong>: {info[planet][key]} <br>\n")
+    output.write(f"    <img class=\"planet-image\" id={planet}-image src=\"images/{planet}.png\"></div>")
